fed0780529ea7b315ac4cb9278f041bff289863c
Lucas Mercurio <lucas10roberto10@gmail.com>
2026-03-27 22:21:58 -0300

    Addin loading bar and testinig

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import Loader from './pages/loader/index'
 
-function App() {}
+function App() {
+    return <Loader />
+}
 
 export default App
